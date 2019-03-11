ac0674451dc4a5f549fd04a4e1656fd86491ea74
roselin-agnelo <48307944+roselin-agnelo@users.noreply.github.com>
2019-03-11 21:42:27 +0530

    initial Commit

---
+++ Assets/scrip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrip : MonoBehaviour
+{
+    public Text Textbox;
+    // Start is called before the first frame update
+    public void plusClick()
+    {
+        Debug.Log("Here");
+        Textbox.text= "1";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
